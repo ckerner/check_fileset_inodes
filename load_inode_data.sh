c2bdad186eee71d44b55a531e22116b2e3351d2b
root <root@gpfs20.ncsa.illinois.edu>
2018-11-27 14:29:30 -0600

    fixed error in load_inode_data.sh where set-analytics was getting set recursively to set-analytics-2 then -2-2 -2-2-2 etc.

--- load_inode_data.sh
+++ load_inode_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 CFICFG=/etc/cfi/cfi.cfg
 
 CLUSTER=$(grep 'gpfs_cluster' ${CFICFG} | awk '{print($3)}')
@@ -11,12 +13,27 @@ FILES="${FILEDIR}/${CLUSTER}.${METRIC}"
 for F in $(ls ${FILES}*)
     do chmod +x ${F}
     #echo ${F}
+
+    # Make a copy to send to set-analytics-2
+    cat ${F} | sed -e 's/set-analytics.ncsa.illinois.edu/set-analytics-2.ncsa.illinois.edu/g' > ${F}.2
+    chmod +x ${F}.2
+
+    # Send the file to set-analytics
     ${F}
     RC=$?
     if [ ${RC} -gt 0 ] ; then
        echo "Error: ${RC} on ${F}"
     else
        rm -f ${F}
+    fi
+
+    # Send the file to set-analytics-2
+    ${F}.2
+    RC=$?
+    if [ ${RC} -gt 0 ] ; then
+       echo "Error: ${RC} on ${F}.2"
+    else
+       rm -f ${F}.2
     fi
 done
 
